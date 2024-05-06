4a4bc6ddbfd74d55a96739416dfeea25aa22998c
andre <deco.xd@outlook.com>
2024-05-06 10:03:04 -0300

    audience section added

--- src/components/shared/AudienceCard.tsx
+++ src/components/shared/AudienceCard.tsx
@@ -6,7 +6,7 @@ type AudienceCardProps = {
 const AudienceCard = ({title,content}:AudienceCardProps) => {
   return (
     <div className="flex justify-between w-full items-center sm:flex-row flex-col  ">
-      <h4 className="text-xl font-medium font-poppins">{title}</h4>
+      <h4 className="text-xl font-medium font-poppins text-center">{title}</h4>
       <div className=" border-l-2 font-montserrat bg-[#FFFEFA] border-l-[#6c3e04] max-w-96 p-4">
         <p>{content}</p>
       </div>
