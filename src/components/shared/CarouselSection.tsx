764db4f6ddf851c070a7eb07b3ffe7fdbd6cff32
andre <deco.xd@outlook.com>
2024-05-11 13:32:16 -0300

    falta apenas adcionar o video

--- src/components/shared/CarouselSection.tsx
+++ src/components/shared/CarouselSection.tsx
@@ -4,6 +4,7 @@ import {
   CarouselContent,
   CarouselItem,
   
+  
 } from "@/components/ui/carousel"
 import { carouselItems } from "@/constants"
 import React from "react"
@@ -17,7 +18,7 @@ const CarouselSection = () => {
   )
 
   return (
-    <Carousel className=" max-w-6xl w-full m-auto mt-20 mb-20   "
+    <Carousel className=" max-w-6xl w-full m-auto mt-20 mb-20  shadow-lg bg-[#6c3e04]/10 rounded-xl  "
     opts={{
       loop:true,
       direction:'ltr',
@@ -25,19 +26,19 @@ const CarouselSection = () => {
     }}
    plugins={[plugin.current]}
     >
-      <CarouselContent className="w-full">
+      <CarouselContent className="w-full ">
          
          {
           carouselItems.map((item,idx) => 
-            <CarouselItem key={idx} className="md:basis-1/1 lg:basis-1/1">
-               <div className="  h-full max-h-[600px] min-h-96 overflow-hidden">
-                <img src={item.imageSrc} alt={item.alt} width={1000} className=" object-cover w-full h-full"/>
+            <CarouselItem key={idx} className="md:basis-1/1 lg:basis-1/1 ">
+               <div className="  h-full md:max-h-[600px] md:min-h-96 min-h-60 overflow-hidden">
+                <img src={item.imageSrc} alt={item.alt} width={1000} className="  w-full h-full rounded-xl"/>
                </div>
           </CarouselItem>)
          }
     
       </CarouselContent>
-     
+ 
     </Carousel>
   )
 }
--- src/sections/Footer.tsx
+++ src/sections/Footer.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
   return (
     <footer className=" mt-40 bg bg-[#FFFEFA] border-t-[1px] border-black/20 font-montserrat font-medium flex justify-center items-center min-h-20 text-sm">
-      <p>Copyright&copy;André Pinheiro</p>
+      <p>Copyright&copy;<a href="https://wa.me/5571996076480" target="_blank">André Pinheiro <span className="font-bold ">solicite seu site</span></a></p>
     </footer>
   )
 }
