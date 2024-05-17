335c14bb4add672e0b0aed7c30aef5a3ebd130e5
andre <deco.xd@outlook.com>
2024-05-17 08:56:17 -0300

    added some keyframes

--- tailwind.config.js
+++ tailwind.config.js
@@ -67,11 +67,68 @@ module.exports = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        "fade-in-right":{
+          from:{
+           opacity:'0',
+           zIndex:'-1',
+           transform: 'translateX(-150px)',
+           }, 
+          to:{
+           opacity:'1',
+           zIndex:'-1',
+           transform: 'translateX(0px)',
+            
+         }
+        },
+        "fade-in":{
+          from:{
+           opacity:'0',
+           zIndex:'-1',
+           transform: 'translateX(150px)',
+           transform: 'translateX(150px)',}, 
+          to:{
+           opacity:'1',
+           zIndex:'-1',
+           transform: 'translateX(0px)',
+           transform:' translateX(0px) ' 
+         }, 
+       },
+        "fade-in-up":{
+          from:{
+           opacity:'0',
+           
+           transform: 'translateY(150px)',
+           }, 
+          to:{
+           opacity:'1',
+           
+           transform: 'translateY(0px)',
+            
+         }, 
+       },
+       "fade-out":{
+        from:{
+         opacity:'1',
+         
+        }, 
+        to:{
+         opacity:'0',
+         
+       }, 
+     },
+        
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        "fade-in":'fade-in .5s .5s linear backwards ',
+        "fade-in-right":'fade-in-right .5s .5s linear backwards ',
+        "fade-in-up":'fade-in-up .5s .6s linear forwards',
+        "fade-out":'fade-out .5s .6s linear',
+
+        
       },
+
     },
   },
   plugins: [require("tailwindcss-animate")],
